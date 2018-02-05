6babc3e8f2a24b65b3b974f01a346bd350d89931
arieby <33897144+arieby@users.noreply.github.com>
2018-02-05 16:58:30 +0200

    Update and rename ntl.js to aman.js

---
+++ aman.js
@@ -0,0 +1,12 @@
+<div class="" id="yui_3_17_2_3_1517842129144_34">
+<div class="col-md-12" id="yui_3_17_2_3_1517842129144_33">
+<div id="rn_ChatLaunchButton_37" class="rn_ChatLaunchButton rn_FormSubmit">
+<button type="submit" id="rn_ChatLaunchButton_37_Button">
+    إرسال    </button>
+    <span class="rn_Hidden">
+        <input id="rn_ChatLaunchButton_37_Submission" type="checkbox" class="rn_Hidden">
+        <label for="rn_ChatLaunchButton_37_Submission" class="rn_Hidden">&nbsp;</label>
+    </span>
+</div>
+</div>
+</div>
