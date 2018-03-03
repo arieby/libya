8032a4a5e7c0896e40e234e351acd419e9271586
arieby <33897144+arieby@users.noreply.github.com>
2018-03-03 12:26:21 +0200

    Update aman.js

--- aman.js
+++ aman.js
@@ -1,5 +1,4 @@
 <div class="" id="yui_3_17_2_3_1517842129144_34">
 <div class="col-md-12" id="yui_3_17_2_3_1517842129144_33">
 <div id="rn_ChatLaunchButton_37" class="rn_ChatLaunchButton rn_FormSubmit">
-<button type="submit" id="rn_ChatLaunchButton_37_Button">
-    إرسال    </button>
+<button type="submit" id="rn_ChatLaunchButton_37_Button">إرسال</button>
